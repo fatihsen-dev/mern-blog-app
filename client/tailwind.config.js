6e14a6eec33695c5fce003745aae1f3bf95a4296
Fatih Şen <fatihydrm@outlook.com>
2022-10-17 03:25:05 +0300

    Add new 2 color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
    content: ["./src/**/*.{js,jsx,ts,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            light: "#f2f2f2",
+            gray: "#858585",
+         },
+      },
    },
    plugins: [],
 };
